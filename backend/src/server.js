600a4d9d89e1098e9dce1c85d7d84b737e2aad81
harshit <monumou912@gmail.com>
2023-11-22 15:55:06 +0530

    api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const specialtiesRoutes = require('./routes/specialtiesRoutes');
-const doctorsRoutes = require('./routes/doctorsRoutes');
+const express = require("express");
+const cors = require("cors");
+const specialtiesRoutes = require("./routes/specialtiesRoutes");
+const doctorsRoutes = require("./routes/doctorsRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-
 app.use(express.json());
 
-app.use('/specialties', specialtiesRoutes);
-app.use('/doctors', doctorsRoutes);
+app.use("/specialties", specialtiesRoutes);
+app.use("/doctors", doctorsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello, welcome to the Doctor Directory!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
